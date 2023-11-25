097f581b87ff36639c3dabe73efbe6769189109f
ber01 <ber01@naver.com>
2023-11-26 03:41:42 +0900

    refactor(AppConfig): AppConfig 리팩터링

--- src/main/java/me/minkh/core/AppConfig.java
+++ src/main/java/me/minkh/core/AppConfig.java
@@ -1,7 +1,9 @@
 package me.minkh.core;
 
+import me.minkh.core.discount.DiscountPolicy;
 import me.minkh.core.discount.FixDiscountPolicy;
 import me.minkh.core.discount.RateDiscountPolicy;
+import me.minkh.core.member.MemberRepository;
 import me.minkh.core.member.MemberService;
 import me.minkh.core.member.MemberServiceImpl;
 import me.minkh.core.member.MemoryMemberRepository;
@@ -11,12 +13,19 @@ import me.minkh.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
-//        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
